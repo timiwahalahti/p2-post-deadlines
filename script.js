90acf72a89692cb9fcb4cc82bf26253a38ce36fc
Timi Wahalahti <timi.wahalahti@gmail.com>
2018-09-14 13:29:28 +0300

    fix p2 frontend js

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 	} );
 
 	$( document ).on( 'p2_new_post_submit_success', function( event, data ) {
-		var post_deadline = $( 'input#p2-post-deadline' ).val();
+		var post_deadline = $( 'input#p2-post-deadline-datepicker' ).val();
 
 		if ( '' !== post_deadline ) {
 			$.post(
 				ajaxUrl.replace( '?p2ajax=true', '' ), {
 					'action': 'p2post_save_deadline',
 					'post_id': parseInt( data.post_id ),
-					'post_deadline': $( 'input#p2-post-deadline' ).val(),
+					'post_deadline': post_deadline,
 					'p2post_save_deadline_nonce': $( '#p2post_save_deadline_nonce' ).val()
 				}
 			);
 
-			$( 'input#p2-post-deadline' ).val('')
+			$( 'input#p2-post-deadline-datepicker' ).val('')
 		}
 	} )
 } )( jQuery );
